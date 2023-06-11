746a1712cf0a569009a327caa0d3077335187a45
Ludvig-Lundberg <ickeskola@gmail.com>
2023-06-11 18:58:20 +0200

    added 'TodosPage.tsx' page and route to it, also fetches data from server

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navigation from "./components/Navigation";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import TodosPage from "./pages/TodosPage";
 
 function App() {
 	return (
@@ -16,6 +17,8 @@ function App() {
 					{/* Login / Register */}
 					<Route path="/login" element={<LoginPage />} />
 					<Route path="/register" element={<RegisterPage />} />
+
+					<Route path="/todos" element={<TodosPage />} />
 				</Routes>
 			</main>
 		</>
---
+++ src/pages/TodosPage.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { encoded } from "./LoginPage";
+
+const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+
+const fetchTodos = async () => {
+	const response: any = await axios.get(baseUrl + "/todos", {
+		headers: { Authorization: `basic ${encoded}` },
+	});
+	return response;
+};
+
+const TodosPage = () => {
+	const { isLoading, isError, data, isSuccess } = useQuery("todos", fetchTodos);
+	return (
+		<div className="todos-container">
+			{isLoading && <p>Loading ...</p>}
+
+			{isError && <p>There was an error retreving todos</p>}
+
+			{isSuccess && (
+				<ul>
+					{data.data.data.map((todo: { id: number; title: string }) => (
+						<li key={todo.id}>{todo.title}</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
+};
+
+export default TodosPage;
