6a4c6adf81ef0cd07120379709694dacb2d15a19
Ludvig-Lundberg <ickeskola@gmail.com>
2023-06-11 17:36:24 +0200

    added a basic base64 encoder to send encoded data instead of plaintext

---
+++ src/services/encoder.ts
@@ -0,0 +1,8 @@
+import base64 from "base-64";
+import utf8 from "utf8";
+
+export let encoded: string;
+export const encodeToBase64 = (email: string, password: string) => {
+	let bytes = utf8.encode(email + ":" + password);
+	encoded = base64.encode(bytes);
+};
