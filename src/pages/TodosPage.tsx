99e8ea15e3563bdf0513caed1d4f495eb14558ac
Ludvig-Lundberg <ickeskola@gmail.com>
2023-07-03 00:18:54 +0200

    added react-table from tanstack, and now render the todos in a table in addition to list

--- src/pages/TodosPage.tsx
+++ src/pages/TodosPage.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import { encoded } from "./LoginPage";
+import { createColumnHelper, useReactTable, getCoreRowModel, flexRender } from "@tanstack/react-table";
 
 const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
@@ -11,22 +12,69 @@ const fetchTodos = async () => {
 	return response;
 };
 
+type todo = {
+	id: number;
+	title: string;
+};
+
+const columnHelper = createColumnHelper<todo>();
+const columns = [
+	columnHelper.accessor("id", {
+		header: () => "Id",
+		cell: (info) => info.getValue(),
+	}),
+	columnHelper.accessor("title", {
+		header: () => "Title",
+		cell: (info) => info.getValue(),
+	}),
+];
+
 const TodosPage = () => {
 	const { isLoading, isError, data, isSuccess } = useQuery("todos", fetchTodos);
+
+	const table = useReactTable({
+		data: data.data.data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
 	return (
 		<div className="todos-container">
 			{isLoading && <p>Loading ...</p>}
-
 			{isError && <p>There was an error retreving todos</p>}
 
 			{isSuccess && (
-				<ul>
-					{data.data.data.map((todo: { id: number; title: string }) => (
-						<li className="todo" key={todo.id}>
-							{todo.title}
-						</li>
-					))}
-				</ul>
+				<>
+					<h2>table</h2>
+					<table>
+						<thead>
+							{table.getHeaderGroups().map((headerGroup) => (
+								<tr key={headerGroup.id}>
+									{headerGroup.headers.map((header) => (
+										<th key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</th>
+									))}
+								</tr>
+							))}
+						</thead>
+						<tbody>
+							{table.getRowModel().rows.map((row) => (
+								<tr key={row.id}>
+									{row.getVisibleCells().map((cell) => (
+										<td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+									))}
+								</tr>
+							))}
+						</tbody>
+					</table>
+					<h2>ul</h2>
+					<ul>
+						{data.data.data.map((todo: { id: number; title: string }) => (
+							<li className="todo" key={todo.id}>
+								{todo.title}
+							</li>
+						))}
+					</ul>
+				</>
 			)}
 		</div>
 	);
