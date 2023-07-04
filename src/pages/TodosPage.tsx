49e52c5c89f7553ffd26fa01407ab98553617267
Ludvig-Lundberg <ickeskola@gmail.com>
2023-07-04 08:10:21 +0200

    fixed bug where data could be undefined by moving table to its own component

--- src/pages/TodosPage.tsx
+++ src/pages/TodosPage.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import { encoded } from "./LoginPage";
-import { createColumnHelper, useReactTable, getCoreRowModel, flexRender } from "@tanstack/react-table";
+import TodosTable from "../components/TodosTable";
 
 const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
@@ -12,31 +12,8 @@ const fetchTodos = async () => {
 	return response;
 };
 
-type todo = {
-	id: number;
-	title: string;
-};
-
-const columnHelper = createColumnHelper<todo>();
-const columns = [
-	columnHelper.accessor("id", {
-		header: () => "Id",
-		cell: (info) => info.getValue(),
-	}),
-	columnHelper.accessor("title", {
-		header: () => "Title",
-		cell: (info) => info.getValue(),
-	}),
-];
-
 const TodosPage = () => {
 	const { isLoading, isError, data, isSuccess } = useQuery("todos", fetchTodos);
-
-	const table = useReactTable({
-		data: data.data.data,
-		columns,
-		getCoreRowModel: getCoreRowModel(),
-	});
 
 	return (
 		<div className="todos-container">
@@ -45,27 +22,17 @@ const TodosPage = () => {
 
 			{isSuccess && (
 				<>
-					<h2>table</h2>
-					<table>
-						<thead>
-							{table.getHeaderGroups().map((headerGroup) => (
-								<tr key={headerGroup.id}>
-									{headerGroup.headers.map((header) => (
-										<th key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</th>
-									))}
-								</tr>
-							))}
-						</thead>
-						<tbody>
-							{table.getRowModel().rows.map((row) => (
-								<tr key={row.id}>
-									{row.getVisibleCells().map((cell) => (
-										<td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
-									))}
-								</tr>
-							))}
-						</tbody>
-					</table>
+					{data.data.data ? (
+						<>
+							<h2>Table</h2>
+							<TodosTable todos={data.data.data} />
+						</>
+					) : (
+						<>
+							<h2>Table</h2>
+							<div>Could not load table</div>
+						</>
+					)}
 					<h2>ul</h2>
 					<ul>
 						{data.data.data.map((todo: { id: number; title: string }) => (
